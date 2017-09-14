c8d06b958c63b97c41dfcb5569f4942194097c06
Malcolm Cowe <malkolm@gmail.com>
2017-09-14 17:27:29 +1000

    Add support for CentOS 7.4, ansd restructure the repository in anticipation of future updates

--- centos-lustre-templates/scripts/download-lustre-e2fsprogs-el7.sh
+++ centos-lustre-templates/scripts/download-lustre-e2fsprogs-el7.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Download the Lustre e2fsprogs packages for EL7 distributions
 wget -N -r -np -l 1 -nd --accept .rpm --reject "*debuginfo*" \
--P rpms/lustre-e2fsprogs/el7 \
+-P ../rpms/lustre-e2fsprogs/el7 \
 https://downloads.hpdd.intel.com/public/e2fsprogs/latest/el7/RPMS/x86_64
